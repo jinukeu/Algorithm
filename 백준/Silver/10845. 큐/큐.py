50fee8cbb26bf6a357eddcf32412af955ffa35b6
이진욱 <koownij@kakao.com>
2022-03-10 12:31:07 +0900

    [Silver IV] Title: 큐, Time: 112 ms, Memory: 32084 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,34 @@
+from sys import stdin
+from collections import deque
+n = int(input())
+
+idx = 0
+stack = deque()
+for _ in range(n):
+      command = list(stdin.readline().split())
+      if command[0] == 'push':
+            stack.append(int(command[1]))
+      if command[0] == 'pop':
+            if len(stack) != 0:
+                  print(stack.popleft())
+            else:
+                  print(-1)
+      if command[0] == 'size':
+            print(len(stack))
+      if command[0] == 'empty':
+            if len(stack) != 0:
+                  print(0)
+            else:
+                  print(1)
+      if command[0] == 'front':
+            if len(stack) != 0:
+                  print(stack[0])
+            else:
+                  print(-1)
+      
+      if command[0] == 'back':
+            if len(stack) != 0:
+                  print(stack[len(stack) - 1])
+            else:
+                  print(-1)
+
