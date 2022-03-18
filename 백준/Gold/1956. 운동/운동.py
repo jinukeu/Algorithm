0c4797bffdbec57d47faa0201a6894c9d223c57e
이진욱 <koownij@kakao.com>
2022-03-18 22:38:35 +0900

    [Gold IV] Title: 운동, Time: 1188 ms, Memory: 129176 KB -BaekjoonHub

---
+++ 백준/Gold/1956. 운동/운동.py
@@ -0,0 +1,30 @@
+import heapq
+
+v, e = map(int,input().split())
+graph = [[1e9] * (v + 1) for _ in range(v + 1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+for i in range(1, v + 1):
+    for j in range(1, v + 1):
+        if i == j:
+            graph[i][j] = 0
+
+for k in range(1, v + 1):
+    for i in range(1, v + 1):
+        for j in range(1, v + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+ans = 1e9
+for i in range(1, v + 1):
+    for j in range(1, v + 1):
+        if graph[i][j] != 0 and graph[i][j] != 1e9 and graph[j][i] != 0 and graph[j][i] != 1e9:
+            ans = min(graph[i][j] + graph[j][i], ans)
+
+if ans == 1e9:
+    print(-1)
+else:
+    print(ans)
+        
