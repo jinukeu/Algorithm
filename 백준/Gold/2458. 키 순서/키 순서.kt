90951c5fae54c6c2ec7b1b5742aee23203899400
이진욱 <koownij@kakao.com>
2022-03-23 16:53:53 +0900

    [Gold IV] Title: 키 순서, Time: 660 ms, Memory: 55364 KB -BaekjoonHub

--- 백준/Gold/2458. 키 순서/키 순서.kt
+++ 백준/Gold/2458. 키 순서/키 순서.kt
@@ -5,30 +5,28 @@ class Main {}
 
 fun main(args: Array<String>) {
     val (n, m) = readln().split(" ").map {it.toInt()}
-    val array = arrayListOf<Array<Int>>()
-    repeat(n + 1) { array.add(Array<Int>(n + 1) { 0 }) }
+    val array = Array(n + 1) { IntArray(n + 1) }
     repeat(m) {
         val (a, b) = readln().split(" ").map {it.toInt()}
         array[a][b] = 1
-        array[b][a] = 2
     }
 
     for(k in 1..n)
-        for(i in 1..n)
-            for(j in 1..n) {
-                if(i == j) continue
-                if(array[i][k] == 1 && array[k][j] == 1)
+        for(i in 1..n) {
+            array[i][i] = 1
+            for (j in 1..n) {
+                if (array[i][k] == 1 && array[k][j] == 1)
                     array[i][j] = 1
-                if(array[i][k] == 2 && array[k][j] == 2)
-                    array[i][j] = 2
             }
+        }
     var cnt = 0
-
-    array.forEach { a ->
-        if((a.count {
-            it == 0
-        }) == 2)
-            cnt ++
+    for(i in 1..n){
+        for(j in 1..n){
+            if(array[i][j] == 0){
+                if(array[j][i] == 0) break
+            }
+            if(j==n)    cnt++
+        }
     }
     print(cnt)
 }
