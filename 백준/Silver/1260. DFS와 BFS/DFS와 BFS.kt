8ef818a059e6ff53d20ccf144acf603b1e08423a
이진욱 <koownij@kakao.com>
2023-01-16 12:17:34 +0900

    [Silver II] Title: DFS와 BFS, Time: 404 ms, Memory: 62476 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.kt
@@ -0,0 +1,58 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+private val br = BufferedReader(InputStreamReader(System.`in`))
+private lateinit var graph: Array<MutableList<Int>>
+private lateinit var visited: Array<Boolean>
+private var answerDfs = ""
+private var answerBfs = ""
+
+fun main(args: Array<String>) {
+    val (n, m, v) = br.readLine().split(" ").map { it.toInt() }
+    graph = Array(n + 1) { mutableListOf() }
+    repeat(m) {
+        val (a, b) = br.readLine().split(" ").map { it.toInt() }
+        graph[a].add(b)
+        graph[b].add(a)
+    }
+
+    repeat(n + 1) { idx ->
+        graph[idx].sort()
+    }
+
+    visited = Array(n + 1) { false }
+    dfs(v)
+    println(answerDfs)
+
+    visited = Array(n + 1) { false }
+    bfs(v)
+    println(answerBfs)
+}
+
+fun dfs(start: Int) {
+    if(visited[start]) return
+
+    visited[start] = true
+    answerDfs += "$start "
+
+    for(next in graph[start]) {
+        dfs(next)
+    }
+}
+
+fun bfs(start: Int) {
+    val queue = ArrayDeque<Int>()
+    queue.add(start)
+
+    while (queue.isNotEmpty()) {
+        val next = queue.removeFirst()
+        if(visited[next]) continue
+        visited[next] = true
+
+        answerBfs += "$next "
+
+        for(node in graph[next]) {
+            queue.add(node)
+        }
+    }
+}
